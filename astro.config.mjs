873c9f8f41fef3e87aa37bd404ef3480f3d50ffc
ib941 <aalkbsy956@gmail.com>
2026-06-29 09:52:16 +0300

    fix: replace netlify adapter with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-output: 'server',
-adapter: netlify(),
-integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind()],
 });
